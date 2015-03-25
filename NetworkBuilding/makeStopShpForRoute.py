1e3164c2291d7b6ddbde9fca684e46e8bbd3c452
hongmou <zhanghongmou@gmail.com>
2015-03-25 10:54:02 -0400

    Recovered the stop list for route code...

---
+++ NetworkBuilding/makeStopShpForRoute.py
@@ -0,0 +1,37 @@
+'''
+Created on Mar 24, 2015
+@author: Hongmou
+@summary: This py splits the stop shapefile for each bus route
+'''
+
+import arcpy, csv
+
+stopShapefile = 'C:/Users/Hongmou/Dropbox (MIT)/HubCity_Hack/Shapefiles/Bus/MBTABUSSTOPS_PT.shp'
+routeShapefile = 'C:/Users/Hongmou/Dropbox (MIT)/HubCity_Hack/Shapefiles/Bus/MBTABUSROUTES_ARC.shp'
+stopListTable = 'C:/Users/Hongmou/Dropbox (MIT)/HubCity_Hack/Data/Bus/StopListForRoute/routeStopCount_HM.csv'
+outputFolder = 'C:/Projects/MassHackathon/stopsForRoutes/'
+
+# CAUTION: may overwrite the current shapefiles in the output folder
+arcpy.env.overwriteOutput = True
+
+with open(stopListTable, 'rb') as f:
+    rd = csv.reader(f)
+    row = rd.next()
+    routeId = row.index('ROUTE_ID')
+    stopListId = row.index('STOP_LIST')
+    # Add all bus stops to a Layer
+    arcpy.MakeFeatureLayer_management(stopShapefile, 'stopLayer')
+    # Add all routes to a layer
+    arcpy.MakeFeatureLayer_management(routeShapefile, 'routeLayer')
+    # For each route
+    for row in rd:
+        stopListStr = row[stopListId]
+        stopList = stopListStr.replace('\'', '').replace('[','(').replace(']',')')
+        # Select the stops of this route from the stop layer
+        arcpy.SelectLayerByAttribute_management('stopLayer', 'NEW_SELECTION', 'STOP_ID IN '+ stopList)
+        # Export the selection to a new shapefile
+        arcpy.CopyFeatures_management("stopLayer", outputFolder + 'routePt' + row[routeId])
+        # Select the route from the route layer
+        arcpy.SelectLayerByAttribute_management('routeLayer', 'NEW_SELECTION', 'CTPS_ROU_2 = '+ row[routeId])
+        # Export the route to a new shapefile
+        arcpy.CopyFeatures_management("routeLayer", outputFolder + 'routeAc' + row[routeId])
